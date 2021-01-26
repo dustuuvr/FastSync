d2ee7e0ec8153dca27451925b9a15f20beae55fa
Dustuu <dustuuvr@gmail.com>
2021-01-26 12:27:58 -0500

    Added Unity project

---
+++ Assets/FastSync/Examples/Scripts/Calculator.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Dustuu.VRChat.FastSync.Examples
+{
+    public class Calculator : UdonSharpBehaviour
+    {
+        [SerializeField] private Text synced;
+        [SerializeField] private Text local;
+        [SerializeField] private Text input;
+        [SerializeField] private SyncInt syncInt;
+        private string inputString = string.Empty;
+
+        protected void Update()
+        {
+            synced.text = $"UdonSync: {syncInt.GetUdonSynced()}";
+            local.text = $"FastSync: {syncInt.GetFastSynced()}";
+            input.text = $"Input: {GetInputInteger().ToString()}";
+        }
+
+        private int GetInputInteger() { return string.IsNullOrEmpty(inputString) ? 0 : int.Parse(inputString); }
+
+        public bool InputStringIsFull() { return inputString.Length >= 5; }
+
+        public void AppendInput(int append)
+        {
+            if (!InputStringIsFull() && append >= 0 && append <= 9 && (append != 0 || inputString.Length > 0))
+            { inputString += append; }
+        }
+
+        // public void BackspaceInput() { if (inputString.Length > 0) { inputString = inputString.Substring(0, inputString.Length - 1); } }
+
+        public void ClearInput() { inputString = string.Empty; }
+
+        public void Add() { ApplyOperation(GetInputInteger()); }
+        public void Subtract() { ApplyOperation(-GetInputInteger()); }
+        private void ApplyOperation(int change)
+        {
+            int operationResult = syncInt.GetFastSynced() + change;
+            syncInt.RequestInt(Mathf.Clamp(operationResult, 0, 9999999));
+            ClearInput();
+        }
+    }
+}
---
+++ Assets/FastSync/Scripts/SyncInt.cs
@@ -0,0 +1,53 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Dustuu.VRChat.FastSync
+{
+    public class SyncInt : UdonSharpBehaviour
+    {
+        private SyncByte[] syncBytes;
+
+        protected void Start() { syncBytes = GetComponentsInChildren<SyncByte>(); }
+
+        // Call this method to request a new value for the SyncInt
+        public void RequestInt(int request)
+        {
+            byte[] requestBytes = Int32ToBytes(request);
+            if (syncBytes.Length == requestBytes.Length)
+            {
+                for (int i = 0; i < syncBytes.Length; i++) { syncBytes[i].RequestByte(requestBytes[i]); }
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Convert");
+            }
+            else { Debug.LogError("[FastSync] request bytes length mismatch!"); }
+        }
+
+        // Called from RequestInt via SendCustomNetworkEvent
+        public void Convert() { foreach (SyncByte syncByte in syncBytes) { syncByte.Convert(); } }
+
+        public int GetUdonSynced() { return GetSynced(false); }
+        public int GetFastSynced() { return GetSynced(true); }
+        private int GetSynced(bool fast)
+        {
+            byte[] bytes = new byte[syncBytes.Length];
+            for (int i = 0; i < bytes.Length; i++) { bytes[i] = fast ? syncBytes[i].GetFastSynced() : syncBytes[i].GetUdonSynced(); }
+            return BytesToInt32(bytes);
+        }
+
+        // Assumes Big-Endian order
+        private int BytesToInt32(byte[] inputBytes) { return (inputBytes[0] << 24) | (inputBytes[1] << 16) | (inputBytes[2] << 8) | (inputBytes[3]); }
+
+        // Assumes Big-Endian order
+        private byte[] Int32ToBytes(int inputInt32)
+        {
+            byte[] bytes = new byte[4];
+            bytes[0] = (byte)((inputInt32 >> 24) % 256);
+            bytes[1] = (byte)((inputInt32 >> 16) % 256);
+            bytes[2] = (byte)((inputInt32 >> 8) % 256);
+            bytes[3] = (byte)(inputInt32 % 256);
+            return bytes;
+        }
+    }
+}
